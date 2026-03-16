3b20c7006f3effb68ea14f16882e2b6b1b14e741
Healer101015 <josonjonesrids@gmail.com>
2026-03-16 10:23:01 -0300

    menu

---
+++ Menu.ts
@@ -0,0 +1,55 @@
+import * as readlineSync from 'readline-sync';
+let opcao: number;
+
+while (true) {
+
+    console.log('*******************************');
+    console.log('Ecommerce do João');
+    console.log('*******************************');
+    console.log('1 - Cadastrar produto');
+    console.log('2 - Listar produtos');
+    console.log('3 - Buscar produto');
+    console.log('4 - atualizar produto');
+    console.log('5 - deletar produto');
+    console.log('6 - sair');
+    console.log('*******************************');
+
+    opcao = readlineSync.questionInt("entre com a opcao desejada: ");
+
+    if (opcao === 6) {
+        console.log("sistema encerrado")
+        process.exit(0);
+    }
+
+    switch (opcao) {
+        case 1:
+            console.log("cadastrar produto");
+            break;
+
+        case 2:
+            console.log("listar produtos");
+            break;
+
+        case 3:
+            console.log("buscar produto");
+            break;
+
+
+        case 4:
+            console.log("atualizar produto");
+            break;
+
+        case 5:
+            console.log("deletar produto");
+            break;
+
+
+        default:
+            console.log("opção inválida");
+
+    }
+
+
+
+
+}
