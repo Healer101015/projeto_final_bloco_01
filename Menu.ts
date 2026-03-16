f06e59d71a20ff5d9be824b331c890abff74901b
Healer101015 <josonjonesrids@gmail.com>
2026-03-16 12:22:52 -0300

    adição menu

--- Menu.ts
+++ Menu.ts
@@ -1,55 +1,82 @@
 import * as readlineSync from 'readline-sync';
+
+import { ProdutoController } from "./src/controller/ProdutoController";
+import { ProdutoFisico } from "./src/model/ProdutoFisico";
+
+const produtos = new ProdutoController();
+
 let opcao: number;
 
 while (true) {
 
-    console.log('**************************');
-    console.log('Ecommerce do João');
-    console.log('*****************************');
-    console.log('1 Cadastrar produto');
-    console.log('2  Listar produtos');
-    console.log('3  Buscar produto');
-    console.log('4  atualizar produto');
-    console.log('5  deletar produto');
-    console.log('6  sair');
-    console.log('*******************************');
+    console.log("\n**************************************");
+    console.log("        ECOMMERCE DO JOÃO             ");
+    console.log("**************************************");
+    console.log("1 - Cadastrar Produto");
+    console.log("2 - Listar Produtos");
+    console.log("3 - Buscar Produto por ID");
+    console.log("4 - Atualizar Produto");
+    console.log("5 - Deletar Produto");
+    console.log("6 - Sair");
+    console.log("**************************************");
 
-    opcao = readlineSync.questionInt("entre com a opcao desejada: ");
+    opcao = readlineSync.questionInt("Entre com a opcao desejada: ");
 
     if (opcao === 6) {
-        console.log("sistema encerrado")
+        console.log("\nSistema encerrado!");
         process.exit(0);
     }
 
     switch (opcao) {
+
         case 1:
-            console.log("cadastrar produto");
+
+            let nome = readlineSync.question("\nNome do produto: ");
+            let preco = readlineSync.questionFloat("Preco: ");
+            let peso = readlineSync.questionFloat("Peso: ");
+
+            produtos.cadastrar(
+                new ProdutoFisico(produtos.gerarId(), nome, preco, peso)
+            );
+
             break;
 
         case 2:
-            console.log("listar produtos");
+
+            produtos.listarTodos();
+
             break;
 
         case 3:
-            console.log("buscar produto");
+
+            let idBusca = readlineSync.questionInt("\nDigite o ID do produto: ");
+            produtos.buscarPorId(idBusca);
+
             break;
 
-
         case 4:
-            console.log("atualizar produto");
+
+            let idAtualizar = readlineSync.questionInt("\nID do produto: ");
+            let nomeNovo = readlineSync.question("Novo nome: ");
+            let precoNovo = readlineSync.questionFloat("Novo preco: ");
+            let pesoNovo = readlineSync.questionFloat("Novo peso: ");
+
+            produtos.atualizar(
+                new ProdutoFisico(idAtualizar, nomeNovo, precoNovo, pesoNovo)
+            );
+
             break;
 
         case 5:
-            console.log("deletar produto");
+
+            let idDeletar = readlineSync.questionInt("\nID do produto: ");
+            produtos.deletar(idDeletar);
+
             break;
 
-
         default:
-            console.log("opção inválida");
+            console.log("\nOpcao invalida!");
 
     }
-
-
-
 
 }
